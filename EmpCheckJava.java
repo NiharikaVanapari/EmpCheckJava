722fe708c0f2833aa3ecd53634b70315169fa105
NiharikaVanapari <niharikavanapari23@gmail.com>
2021-06-22 00:47:23 +0530

    ConditionalWages

--- EmpCheckJava.java
+++ EmpCheckJava.java
@@ -4,13 +4,19 @@ class EmpCheckJava
 public static void main(String[] args)
 {
 int full_day =8;
-int wage_per_hour=20;
+int wage_per_hour=5;
 int daily_wage;
 int part_time=4;
 int num_of_workingdays=20;
+int max_hrs_in_month=100;
+int emphrs=0, totEmphrs=0, totWorkingdays=0;
 System.out.println("Welcome to Employee Wage Computation");
-for(int day=0;day<num_of_workingdays;day++)
+while(totEmphrs<=max_hrs_in_month && totWorkingdays<num_of_workingdays)
 {
+totWorkingdays++;
+
+//for(int day=0;day<num_of_workingdays;day++)
+//{
 Random random = new Random();
 int random1 = random.nextInt(3);
 //if(random1 == 1)
@@ -30,18 +36,23 @@ int random1 = random.nextInt(3);
 switch(random1)
 {
 case 1:
-daily_wage=full_day*wage_per_hour;
-System.out.println("Employee is Present ");
-System.out.println("Employee wage is " +daily_wage);
+emphrs=8;
+
+System.out.println("Employee hours is " +emphrs);
 break;
 case 2:
-daily_wage=part_time*wage_per_hour;
-System.out.println("Employee is Part time ");
-System.out.println("Employee wage is " +daily_wage);
+emphrs=4;
+
+System.out.println("Employee wage is " +emphrs);
 break;
 default:
 System.out.println("Employee is Absent");
 }
+totEmphrs=emphrs;
+System.out.println("Day#: "+totWorkingdays +" Emp hr: "+emphrs);
+
+}
+
 }
 }
-}
+
