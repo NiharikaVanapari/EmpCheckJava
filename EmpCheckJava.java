6d844068e248bf65e9205fe70154e6ca592e0cef
NiharikaVanapari <niharikavanapari23@gmail.com>
2021-06-21 21:56:07 +0530

    EmpCheck

--- EmpCheckJava.java
+++ EmpCheckJava.java
@@ -1,7 +1,14 @@
+import java.util.Random;
 class EmpCheckJava
 {
 public static void main(String[] args)
 {
 System.out.println("Welcome to Employee Wage Computation");
+Random random = new Random();
+int random1 = random.nextInt(2);
+if(random1 == 1)
+System.out.println("Employee is Absent");
+else
+System.out.println("Employee is Present");
 }
 }
