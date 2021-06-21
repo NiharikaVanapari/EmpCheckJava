90ae4aabe238e84b4e7eba247635ef26ee2e52d9
NiharikaVanapari <niharikavanapari23@gmail.com>
2021-06-21 22:24:38 +0530

    EmployeeDailyWage

--- EmpCheckJava.java
+++ EmpCheckJava.java
@@ -3,12 +3,21 @@ class EmpCheckJava
 {
 public static void main(String[] args)
 {
+int full_day =8;
+int wage_per_hour=20;
+int daily_wage;
 System.out.println("Welcome to Employee Wage Computation");
 Random random = new Random();
 int random1 = random.nextInt(2);
 if(random1 == 1)
-System.out.println("Employee is Absent");
-else
+{
 System.out.println("Employee is Present");
+daily_wage=full_day*wage_per_hour;
+System.out.println("Employee wage is " +daily_wage);
+
+}
+else
+System.out.println("Employee is Absent");
+
 }
 }
